ba50bf61dbaab7d329f45d94cbc3747fc060a889
Theorun <persh005@gmail.com>
2025-12-24 11:40:02 -0600

    1.2.75

--- exotic-space-industries-remembrance/prototypes/steam_age/cannon-turret-mk1.lua
+++ exotic-space-industries-remembrance/prototypes/steam_age/cannon-turret-mk1.lua
@@ -176,3 +176,32 @@ data:extend({
 },
 }
 )
+
+table.insert(
+	data.raw.technology["physical-projectile-damage-1"].effects,
+	{ type = "turret-attack", turret_id = "ei-cannon-turret-mk1", modifier = 0.05 }
+)
+table.insert(
+	data.raw.technology["physical-projectile-damage-2"].effects,
+	{ type = "turret-attack", turret_id = "ei-cannon-turret-mk1", modifier = 0.05 }
+)
+table.insert(
+	data.raw.technology["physical-projectile-damage-3"].effects,
+	{ type = "turret-attack", turret_id = "ei-cannon-turret-mk1", modifier = 0.05 }
+)
+table.insert(
+	data.raw.technology["physical-projectile-damage-4"].effects,
+	{ type = "turret-attack", turret_id = "ei-cannon-turret-mk1", modifier = 0.1 }
+)
+table.insert(
+	data.raw.technology["physical-projectile-damage-5"].effects,
+	{ type = "turret-attack", turret_id = "ei-cannon-turret-mk1", modifier = 0.1 }
+)
+table.insert(
+	data.raw.technology["physical-projectile-damage-6"].effects,
+	{ type = "turret-attack", turret_id = "ei-cannon-turret-mk1", modifier = 0.1 }
+)
+table.insert(
+	data.raw.technology["physical-projectile-damage-7"].effects,
+	{ type = "turret-attack", turret_id = "ei-cannon-turret-mk1", modifier = 0.2 }
+)
